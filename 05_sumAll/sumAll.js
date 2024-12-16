e7d1d8346f8066d6a68a80823df9130ab3e9dadc
Monty <montydmontilla@gmail.com>
2024-12-16 18:18:35 -0500

    Completed 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,11 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (!(Number.isInteger(first)) || !(Number.isInteger(second))) return 'ERROR';
+    if (first < 0 || second < 0) return 'ERROR';
+    const start = first < second ? first : second;
+    const end = second > first ? second : first;
+    let sum = 0;
+    for (let i = start; i < end +1; i++) sum += i;
+    return sum;
 
 };
 
