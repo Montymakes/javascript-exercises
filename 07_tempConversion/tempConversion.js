6622e21ea1334d60b62ad57b7a17f5a42e548f97
Monty <montydmontilla@gmail.com>
2024-12-16 18:58:27 -0500

    Completed 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+//x°C = (x°F - 32) * 5/9
+const convertToCelsius = function(deg) {
+    return Math.round((deg-32) * (5/9)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+// x°F = x°C * 9/5 +32
+const convertToFahrenheit = function(deg) {
+  return Math.round((deg * (9/5) + 32)*10)/10;
 };
 
 // Do not edit below this line
