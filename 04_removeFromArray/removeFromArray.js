0f7ca43fbacb0da2add817d11779b200feb480fe
Monty <montydmontilla@gmail.com>
2024-12-16 15:12:36 -0500

    Completed 03_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, first, second = undefined, third = undefined, fourth = undefined) {
+    const newArray = array.filter((element)=> {
+        if (element !== first
+            && element !== second
+            && element !== third
+            && element !== fourth
+        ) return element
+    })
+    return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
