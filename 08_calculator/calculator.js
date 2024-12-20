485a16fff81b01e38a4ef2c9741eb461f72fa577
Monty <montydmontilla@gmail.com>
2024-12-19 22:34:36 -0500

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => add(total, current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length < 1) return 0;
+  return	array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  let factorialSum = 1;
+  for (let i = 1; i < num + 1; i++) {
+    factorialSum *= i;
+  }
+  return factorialSum;
 };
 
 // Do not edit below this line
