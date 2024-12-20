3c132c8d3e17dbe26afbd8e74695622d70468d15
Monty <montydmontilla@gmail.com>
2024-12-20 18:28:14 -0500

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+ people.sort((a,b) => {
+   const currentYear = new Date().getFullYear();
+   const aBirth = a.yearOfBirth;
+   const bBirth = b.yearOfBirth;
+   const aDeath = a?.yearOfDeath ? a.yearOfDeath : currentYear;
+   const bDeath = b?.yearOfDeath ? b.yearOfDeath : currentYear;
+  
+   const aAge = aDeath - aBirth;
+   const bAge = bDeath - bBirth;
+      
+   return aAge > bAge ? 1 : -1;
+ })
 
+ return people[people.length -1];
 };
 
 // Do not edit below this line
