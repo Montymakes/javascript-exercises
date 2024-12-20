3c22d7951f9d028b0147e7f7970687e6a957af46
Monty <montydmontilla@gmail.com>
2024-12-19 23:59:42 -0500

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    position = Number(position);
+    if (position < 0) return 'OOPS';
+    const fibonacciNums = [0,1,1]; //0 = 
+    for (let i = 3; i < position + 1; i++) {
+        fibonacciNums[i] = fibonacciNums[i-1] + fibonacciNums[i-2];
+    }
+    return fibonacciNums[position];
 };
-
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
