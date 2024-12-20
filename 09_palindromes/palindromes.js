bba12a3cf761c18d6a9eed871b0350c26b58e86d
Monty <montydmontilla@gmail.com>
2024-12-19 23:18:58 -0500

    Completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const regex = /[^a-zA-Z0-9]/g;
+    const cleanString = string.replace(regex, '').toLowerCase();
+    let reverseString = '';
+    for (let index = cleanString.length -1; index > -1; index--) reverseString += cleanString.charAt(index); 
+    if (reverseString === cleanString) {
+        return true;
+    }
+    return false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
